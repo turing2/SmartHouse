6207f35de57d0885ed7d009d52e5362f1c4c1d1b
zhukewen_java <zhukewen_java@126.com>
2014-05-13 22:51:46 +0800

    add log4j

--- src/main/java/com/daxia/smarthouse/plugin/ec/TV.java
+++ src/main/java/com/daxia/smarthouse/plugin/ec/TV.java
@@ -1,11 +1,11 @@
 package com.daxia.smarthouse.plugin.ec;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.apache.log4j.Logger;
 
 import com.daxia.smarthouse.plugin.AbstractPlugin;
 
 public class TV extends AbstractPlugin {
+    private static Logger logger = Logger.getLogger(TV.class);
     
     private String name = "LeTV 3.0";
     
@@ -21,16 +21,16 @@ public class TV extends AbstractPlugin {
 
     @Override
     public void turnOn() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String log = name + " was turned on at " + dateFormat.format(new Date());
-        System.out.println(log);
+        if (logger.isDebugEnabled()) {
+            logger.debug(getName() + " turned on");
+        }
     }
 
     @Override
     public void turnOff() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String log = name + " was turned off at " + dateFormat.format(new Date());
-        System.out.println(log);
+        if (logger.isDebugEnabled()) {
+            logger.debug(getName() + " turned off");
+        }
     }
 
 }
